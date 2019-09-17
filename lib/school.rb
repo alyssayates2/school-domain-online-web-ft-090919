ce4668e07160167be817c96896ed718d7e55c75c
Alyssa Yates <bosslys@icloud.com>
2019-09-17 22:06:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,26 @@
 class School
+  def initialize(school)
+    @school = school
+  end 
+end
+
+
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = [ ]
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
